4d47344b55788bf00e92ce3aacb6d7ba5acc7502
David Melchor <dmelchornatplat@gmail.com>
2026-02-21 00:07:06 -0600

    feat: FastAPI + PostgreSQL + Docker

--- app/core/database.py
+++ app/core/database.py
@@ -8,8 +8,8 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # FIX Render
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
 engine = create_async_engine(
     DATABASE_URL,
